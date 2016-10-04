4159be7115d0b4884c0f1761d84e64489ba60016
Adam Nawrocki <adam.nawroc@gmail.com>
2016-10-04 08:27:26 -0400

    new data on every button click

---
+++ app.js
@@ -0,0 +1,31 @@
+var go = function() {
+
+//   $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+
+
+
+// });
+
+  tagged_url = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback="
+// var request = {
+//     // tagged: tags,
+//     site: 'stackoverflow'
+//     // order: 'desc',
+//     // sort: 'creation'
+//   };
+  $.ajax({
+    url: tagged_url,
+    cache: false,
+    // data: request,
+    dataType: "json",//use jsonp to avoid cross origin issues
+    type: "GET",
+  })
+  .done(function(a){ //this waits for the ajax to return with a succesful promise object
+      $("body").append("<p>" + a[0].title + "</p>")
+  })
+
+}
+
+$("button").click(function() {
+  go();
+})
