5d907d54e3fb2b4d56a116f56b0298235695cb7f
Adam Nawrocki <adam.nawroc@gmail.com>
2016-10-10 07:45:18 -0400

    diff syntax

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var go = function() {
   var randomColor = Math.floor(Math.random()*16777215).toString(16);
   console.log(randomColor);
-  $("body").css("background-color", "#" + randomColor)
+  $("body").css({"background-color": "#" + randomColor})
 
   tagged_url = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
 // var request = {
